d592a32ec3f9ac7883b572d639e5661a88fd7642
Андрей Изотов <andrej.izotoff@yandex.ru>
2022-03-24 22:41:03 +0300

    24.03.2022 first commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,15 +1,19 @@
+from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'title', 'slug', 'description')
+        model = Group
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'author', 'text', 'pub_date', 'image', 'group')
         model = Post
 
 
@@ -19,5 +23,37 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'author', 'text', 'created', 'post')
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+
+    def validate(self, data):
+        user = self.context['request'].user
+        following = data['following']
+        if user == following:
+            raise serializers.ValidationError(
+                'Подписываться на себя нельзя!'
+            )
+        if Follow.objects.filter(
+            user=User.objects.get(username=user),
+            following=User.objects.get(username=following)
+        ).exists():
+            raise serializers.ValidationError(
+                'Вы уже подписались'
+            )
+        return data
